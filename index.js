2a1907419a39fdbfcf31e7942b8a9fee85939360
Colton Wolkins (Laptop) <colton@indicio.tech>
2022-02-11 23:59:17 -0700

    feat: Prettyprint non-pino log messages

--- index.js
+++ index.js
@@ -16,13 +16,56 @@ var history = [];
 
 const Syslog = require('simple-syslog-server');
 var syslogServer = Syslog.UDP({type: 'udp4'});
+
+function getLevel(code) {
+	switch(code) {
+		case 0:
+		case 1:
+		case 2:
+			return 60;
+		case 3:
+			return 50;
+		case 4:
+			return 40;
+		case 5:
+			return 30;
+		case 6:
+			return 20;
+		case 7:
+		default:
+			return 10;
+	}
+}
+
+function emitLogMessage(conn, logEntry) {
+	let isJSON = false;
+	let msg = logEntry.msg;
+	try {
+		JSON.parse(msg);
+		isJSON = true;
+	} catch(err) {}
+	if(!isJSON) {
+		let level = getLevel(logEntry.severityCode);
+		msg = JSON.stringify(
+			{
+				level: level,
+				time: new Date(logEntry.timestamp).getTime(),
+				hostname: logEntry.hostname,
+				ns: logEntry.tag,
+				msg: logEntry.msg,
+			}
+		);
+	}
+	conn.emit('chat message', msg);
+}
+
 syslogServer.on('msg', data => {
 	console.log(data);
 	history.push(data);
 	console.log(history.length);
 	while(history.length > 3000)
 		history.shift();
-	io.emit('chat message', data.msg);
+	emitLogMessage(io, data);
 });
 syslogServer.on('invalid', err => {
 	console.warn('Invalid message format received: %o\n', err) ;
@@ -46,7 +89,7 @@ io.on('connection', socket => {
 	//socket.broadcast.emit('connected');
 	io.emit('chat message', 'connected');
 	for(let line of history)
-		socket.emit('chat message', line.msg);
+		emitLogMessage(socket, line);
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
 	});
